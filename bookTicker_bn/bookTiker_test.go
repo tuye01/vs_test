231f287e8ff2756d29ae1acc0410901b9677f5d8
tuye01 <16337093682@qq.com>
2025-03-15 00:31:30 +0800

    del print

--- bookTicker_bn/bookTiker_test.go
+++ bookTicker_bn/bookTiker_test.go
@@ -1,14 +1,12 @@
 package bookticker_bn
 
 import (
-	"fmt"
 	"log"
 	"testing"
 	"time"
 )
 
 func TestBookTicker(t *testing.T) {
-	fmt.Println("🚀 启动程序")
 	go connectToBinance()
 	go sendHeartbeat() // 启动心跳检测
 	// 运行 2 小时
